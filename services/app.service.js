f29b8cd1d163d56f18a6ec9d78711f7066479781
mansa <deepak057@yahoo.com>
2019-05-28 15:38:05 +0530

    working on feed

--- services/app.service.js
+++ services/app.service.js
@@ -165,7 +165,7 @@ function getWebPost (post, user) {
 
   //add some custom properties 
   post_web.show = true
-  post_web.showComments = true
+  post_web.showComments = false
   
   return post_web;
 }
