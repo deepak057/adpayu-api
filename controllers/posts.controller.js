416a63fc7461888ca7eb5a34c6e4ba1a076fa707
mansa <deepak057@yahoo.com>
2020-02-05 22:58:21 +0530

    working on dynamic feed

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -699,7 +699,7 @@ function smartFeed (posts, user, tag) {
       return Posts.scope(getPostScopes(user)).findAll(criteria)
     }
     let fetchNewPostsAndMerge = (newPosts) => {
-      console.log('\n\n\n\n\n Length new posts- '+ newPosts.length + '\n\n\n\n\n')
+      // console.log('\n\n\n\n\n Length new posts- '+ newPosts.length + '\n\n\n\n\n')
       if (newPosts && newPosts.length) {
         let criteria = getPostCriteriaObject(user)
         criteria.where.id = getPostsIds(newPosts)
@@ -707,7 +707,7 @@ function smartFeed (posts, user, tag) {
           .then((newPosts) => {
             if (newPosts) {
               console.log("\n\n\n\n\n original posts - " + posts.length + ' added Posts -' + newPosts.length +"\n\n")
-              posts.concat(newPosts)
+              posts = posts.concat(newPosts)
             }
             resolve(posts)
           })
@@ -728,13 +728,13 @@ function smartFeed (posts, user, tag) {
           .then((videoPosts) => {
             if (videoPosts) {
               // console.log('\n\n\n\n Videos found ' + videoPosts + '\n\n\n\n\n')
-              postsToAdd.concat(videoPosts)
+              postsToAdd = postsToAdd.concat(videoPosts)
             }
             getRandomPosts(criteria)
               .then((randomPosts) => {
                 if (randomPosts) {
                   // console.log('\n\n\n\n Random Posts found ' + randomPosts + '\n\n\n\n\n')
-                  postsToAdd.concat(randomPosts)
+                  postsToAdd = postsToAdd.concat(randomPosts)
                 }
                 fetchNewPostsAndMerge(postsToAdd)
               })
@@ -746,7 +746,7 @@ function smartFeed (posts, user, tag) {
         getRandomPosts(criteria)
           .then((randomPosts) => {
             if (randomPosts) {
-              postsToAdd.concat(randomPosts)
+              postsToAdd = postsToAdd.concat(randomPosts)
               //console.log('\n\n\n\n Random Posts found ' + randomPosts + '\n\n\n\n\n')
             }
             fetchNewPostsAndMerge(postsToAdd)
